3d04968989029aa06b753e6a37b1a1d33c773d2d
ParijaMalgaonkar <52425907+ParijaMalgaonkar@users.noreply.github.com>
2019-08-12 10:29:47 +0530

    Add files via upload

---
+++ placement.js
@@ -0,0 +1,33 @@
+google.load('visualization', '1', {
+    packages: ['table']
+});
+var visualization;
+
+function drawVisualization() {
+    var query = new google.visualization.Query('https://spreadsheets.google.com/tq?key=1ByoN-7CwOhqdGsU4VCJ9PMdXkHukqz9umVVVgGhmtFU&output=html&usp=sharing');
+    query.setQuery('SELECT A, B label A "Company", B "Job Portal"');
+    query.send(handleQueryResponse);
+}
+
+function handleQueryResponse(response) {
+    if (response.isError()) {
+        alert('There was a problem with your query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+        return;
+    }
+    var data = response.getDataTable();
+    visualization = new google.visualization.Table(document.getElementById('table'));
+    visualization.draw(data, {
+        allowHtml: true,
+        legend: 'bottom'
+    });
+}
+google.setOnLoadCallback(drawVisualization);
+
+/* to put in index.html: 
+  <div id="box">
+    <h1>Try</h1>
+    <script src="google-sheets-to-html.js" type="text/javascript"></script>
+    <div id="table">
+    </div>
+  </div>
+*/
